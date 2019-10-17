e2cf7d9028ae08b04b84713f13731a54e3eae1e9
Mocktezuma <adrian.salas19@gmail.com>
2019-10-17 15:58:16 +0200

    added glava and dunst

---
+++ .config/glava/restart_glava.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+color=$(sed -n 10p ~/.cache/wal/colors)
+	sed -i "s|#define COLOR.*$|#define COLOR ("$color" * ((d / 60) + 1))|g" ~/.config/glava    /bars.glsl
+	sed -i "s|#define COLOR.*$|#define COLOR ("$color" * ((d / 40) + 1))|g" ~/.config/glava    /radial.glsl
+killall -q glava
+sleep 1
+i3-msg " exec --no-startup-id glava --desktop"
