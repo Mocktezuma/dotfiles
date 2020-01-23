8bbf8cd51635becba134d2fa26dc7cd9780c9aa4
Mocktezuma <adrian.salas@protonmail.com>
2020-01-23 08:23:52 +0100

    fixed picom.conf and reverted glava changes

--- .config/glava/rc1.glsl
+++ .config/glava/rc1.glsl
@@ -39,7 +39,7 @@
               Has very little performance impact.
     
    "none"   - Disable window opacity completely. */
-#request setopacity "xroot"
+#request setopacity "native"
 
 /* Whether to mirror left and right audio input channels from PulseAudio.*/
 #request setmirror false
